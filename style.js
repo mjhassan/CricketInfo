761def284f99c3e8b8b38bd028bbe69e8275ad88
Jahid Hassan <jahid.hassan@infolytx.com>
2019-10-17 17:51:23 +0600

    ref for EStyleSheet added

--- style.js
+++ style.js
@@ -2,4 +2,9 @@ import EStyleSheet from 'react-native-extended-stylesheet';
 
 EStyleSheet.build({
   $textColor: '#0275d8',
+  roundButton: {
+    backgroundColor: '#89fe12',
+  },
 });
+
+// https://github.com/vitalets/react-native-extended-stylesheet/blob/master/examples/readme/component.js
